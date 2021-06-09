f40d079bdb744c69455e5d34eaff76f1aa44dd1d
atsushi0919 <hatake0919@yahoo.co.jp>
2021-06-09 17:07:08 +0900

    Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get "/home", to: "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
-  # root "static_pages#home"
-  root "herbs#index"
+  root "static_pages#home"
+  #root "herbs#index"
+  resources :users
   resources :herbs
 end

62c3d9218e6473502f4fdf2424e006634c04cfbf
atsushi0919 <hatake0919@yahoo.co.jp>
2021-06-09 15:31:46 +0900

    Add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/home", to: "static_pages#home"
   get "/about", to: "static_pages#about"
+  get "/signup", to: "users#new"
   # root "static_pages#home"
   root "herbs#index"
   resources :herbs

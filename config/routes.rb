4673baa20e71bb8f8cce2bb77fc2dea868e9718c
atsushi0919 <hatake0919@yahoo.co.jp>
2021-06-14 11:40:42 +0900

    パスワードリセット機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :users
   resources :herbs
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end

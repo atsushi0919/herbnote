5878c1c59f7c9912c660a14696f762c514fa7fd9
atsushi0919 <hatake0919@yahoo.co.jp>
2021-06-21 10:40:14 +0900

    マイグレーションファイルを整理

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,13 @@ ActiveRecord::Schema.define(version: 2021_06_18_065820) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "posted_topic_relations", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "topic_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "posted_topics", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.bigint "herb_property_id", null: false
@@ -89,8 +96,6 @@ ActiveRecord::Schema.define(version: 2021_06_18_065820) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
-    t.integer "herb_id"
-    t.integer "topic_id"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end

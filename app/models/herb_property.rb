81aecb9c88b5413f845616febba8928864690674
atsushi0919 <hatake0919@yahoo.co.jp>
2021-06-21 11:22:24 +0900

    投稿とトピック関連付けの修正

--- app/models/herb_property.rb
+++ app/models/herb_property.rb
@@ -2,6 +2,6 @@ class HerbProperty < ApplicationRecord
   validates :feature, { presence: true, length: { maximum: 20 } }
 
   has_many :herb_property_relations, dependent: :destroy
-  has_many :herbs, through: :herb_property_relations, dependent: :destroy
-  has_many :posts, through: :posted_topics, dependent: :destroy
+  has_many :herbs, through: :herb_property_relations
+  has_many :posts, through: :posted_topics
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :posted_topics, dependent: :destroy
+  has_many :herb_properties, through: :posted_topics
 
   # image_uploader.rb のクラス ImageUploader と posts テーブルの image カラムを連携
   mount_uploader :image, ImageUploader
